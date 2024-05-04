a73620d8a82cf68cc3680d3d6f0f74133a1dd1f1
Ignacio Joaquín Soler <ignaciojsoler@gmail.com>
2024-05-03 21:38:29 -0300

    functions fixed

--- src/clean-code/03-functions.ts
+++ src/clean-code/03-functions.ts
@@ -1,27 +1,31 @@
 (() => {
 
     // función para obtener información de una película por Id
-    function getAllMovies( movieId: string ) {
-        console.log({ movieId });
+    function getMovieById( id: string ) {
+        console.log({ id });
     }
 
     // función para obtener información de los actores de una película - Actors o Cast // id = movieId getMovieCast
-    function getAllMovieActors( id: string ) {
+    function getMovieCastById( id: string ) {
         console.log({ id });
     }
 
     // funcion para obtener el bio del actor por el id
-    function getUsuario( ActorId: string ) {
-        console.log({ ActorId });
+    function getActorBioById( id: string ) {
+        console.log({ id });
+    }
+
+    interface MovieSettings {
+        title: string, description: string, rating: number, cast: string[]
     }
     
     // Crear una película
-    function movie(title: string, description: string, rating: number, cast: string[] ) {
+    function createMovie( { title, cast, description, rating }: MovieSettings ) {
         console.log({ title, description, rating, cast });
     }
 
     // Crea un nuevo actor
-    function createActorIfActorNotExists( fullName: string, birthdate: Date ): boolean {
+    function createActor( fullName: string, birthdate: Date ): boolean {
         
         // tarea asincrona para verificar nombre
         // ..
@@ -33,7 +37,14 @@
 
     }
 
-    
+    const getPayAmount = ({ isDead = false, isSeparated = true, isRetired = false }): number => {
+        
+        if (isDead) return 1500;
+
+        if (isSeparated) return 2500;
+
+        return (isRetired) ? 3000 : 4000;
+    }
 
 
 })();
