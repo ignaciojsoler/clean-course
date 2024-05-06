b5d6dd6af696f58688d46beba65b465d207311f3
Ignacio Joaquín Soler <ignaciojsoler@gmail.com>
2024-05-06 16:45:39 -0300

    OCP excercise

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,10 @@
-import './style.css'
-import './solid/01-srp';
+import "./style.css";
+// import './solid/01-srp';
+import "./solid/02-open-close-a";
 
-const app = document.querySelector<HTMLDivElement>('#app')!
+const app = document.querySelector<HTMLDivElement>("#app")!;
 
 app.innerHTML = `
   <h1>CleanCode y SOLID</h1>
   <span>Revisar la consola de JavaScript</span>
-`
-
+`;
